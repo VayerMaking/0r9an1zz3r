505952bef5fc4ae48f783c998f8fa5717ae3c73b
Martin Vayer <martin.vayer@gmail.com>
2021-12-09 22:40:51 +0200

    convert rgb to color names

---
+++ api/color_name.py
@@ -0,0 +1,17 @@
+from scipy.spatial import KDTree
+from webcolors import css3_hex_to_names, hex_to_rgb
+
+
+def convert_rgb_to_names(rgb_tuple):
+
+    # a dictionary of all the hex and their respective names in css3
+    css3_db = css3_hex_to_names
+    names = []
+    rgb_values = []
+    for color_hex, color_name in css3_db.items():
+        names.append(color_name)
+        rgb_values.append(hex_to_rgb(color_hex))
+
+    kdt_db = KDTree(rgb_values)
+    distance, index = kdt_db.query(rgb_tuple)
+    return f'closest match: {names[index]}'
