0a587271956f01e76b4f847c6b82486b53f7b764
Martin Vayer <martin.vayer@gmail.com>
2021-12-31 17:27:44 +0200

    add new component for displaying categories

--- app/screens/TabTwoScreen.tsx
+++ app/screens/TabTwoScreen.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
+import CategoriesComponent from '../components/CategoriesComponent';
 import { Text, View } from '../components/Themed';
 
 export default function TabTwoScreen() {
@@ -10,6 +11,7 @@ export default function TabTwoScreen() {
       <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <CategoriesComponent />
     </View>
   );
 }
