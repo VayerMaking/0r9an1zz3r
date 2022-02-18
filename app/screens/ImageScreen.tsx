1e94f7eceb9493a98dd5b2b37515e861ce9b83e2
Martin Vayer <martin.vayer@gmail.com>
2022-02-18 09:38:04 +0200

    implement color detection pie chart on FE

--- app/screens/ImageScreen.tsx
+++ app/screens/ImageScreen.tsx
@@ -4,7 +4,7 @@ import {
     useRoute,
 } from "@react-navigation/native";
 import * as React from 'react';
-import { StyleSheet, ScrollView, Image } from 'react-native';
+import { StyleSheet, ScrollView, Image, Dimensions } from 'react-native';
 
 import ImageComponent from '../components/ImageComponent';
 import { Text, View } from '../components/Themed';
@@ -12,53 +12,107 @@ import { RootStackParamList, RootStackScreenProps, RootTabScreenProps } from '..
 import window from '../constants/Layout';
 import { useEffect, useState } from "react";
 import { axiosInstance, urls } from "../utils/auth";
+import { PieChart } from "react-native-chart-kit";
 
-
-export default function ImageScreen({ navigation }: RootStackScreenProps<'Image'>) {
+export default function ImageScreen({
+    navigation,
+}: RootStackScreenProps<"Image">) {
     const route = useRoute();
 
     const baseApiURL = route.params?.baseApiURL;
     const filename = route.params?.filename;
     const imageId = route.params?.imageId;
-    const URL = baseApiURL + '/image/' + filename;
+    const URL = baseApiURL + "/image/" + filename;
+
+    const [dataState, setDataState] = useState<{
+        name: string,
+        color: string,
+        population: number,
+        legendFontColor: string,
+        legendFontSize: number
+    }[]>([]);
 
     interface IImageDetails {
-        colors: string[],
-        filename: string,
-        id: number,
-        tag: string,
+        colors_rgb: string[];
+        colors_hex: string[];
+        color_percentages: number[];
+        filename: string;
+        id: number;
+        tag: string;
     }
 
     let defaultImageDetails: IImageDetails = {
-        colors: [],
-        filename: '',
+        colors_rgb: [],
+        colors_hex: [],
+        color_percentages: [],
+        filename: "",
         id: 0,
-        tag: ''
+        tag: "",
     };
 
-    const [imageDetails, setImageDetails] = useState<IImageDetails>(defaultImageDetails);
+    const [imageDetails, setImageDetails] =
+        useState<IImageDetails>(defaultImageDetails);
+
+    function setData(imgDetails: IImageDetails) {
+        let temp = [...dataState];
+        let sum = imgDetails.color_percentages.reduce(function (a, b) {
+            return a + b;
+        }, 0);
+        for (let i = 0; i < 3; i++) {
+            //console.log(Number((100 * imgDetails.color_percentages[i] / sum).toPrecision(3)));
+            temp.push({
+                name: imgDetails.colors_rgb[i],
+                color: imgDetails.colors_hex[i],
+                population: 100 * imgDetails.color_percentages[i] / sum,
+                legendFontColor: "#7F7F7F",
+                legendFontSize: 15
+            });
+        }
+        return setDataState(temp);
+    }
 
     async function fetchImageDetails() {
         try {
-            const url = baseApiURL + '/getImageDetails/' + imageId;
+            const url = baseApiURL + "/getImageDetails/" + imageId;
             const response = await axiosInstance.get(url);
             const json = await response.data;
             const fetchedImageDetails: IImageDetails = {
-                colors: json.colors,
+                colors_rgb: json.colors_rgb,
+                colors_hex: json.colors_hex,
                 filename: json.filename,
                 id: json.id,
                 tag: json.tag,
-            }
-            setImageDetails(fetchedImageDetails);
-        } catch (err) {
-            console.warn(err)
+                color_percentages: json.color_percentages,
+            };
+            setData(fetchedImageDetails);
 
+            setImageDetails(fetchedImageDetails);
+            console.log(imageDetails);
+        } catch (err) {
+            console.warn(err);
         }
     }
 
     useEffect(() => {
         fetchImageDetails();
     }, []);
+
+    const chartConfig = {
+        backgroundColor: "#e26a00",
+        backgroundGradientFrom: "#fb8c00",
+        backgroundGradientTo: "#ffa726",
+        decimalPlaces: 2, // optional, defaults to 2dp
+        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        style: {
+            borderRadius: 16,
+        },
+        propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#ffa726",
+        },
+    };
 
     return (
         <ScrollView>
@@ -69,13 +123,29 @@ export default function ImageScreen({ navigation }: RootStackScreenProps<'Image'
                 <Text style={styles.detailItem}>{imageDetails.tag}</Text>
                 <Text style={styles.detailItem}>COLORS:</Text>
 
-                {imageDetails.colors.map(color => {
-                    const keyId = imageDetails.colors.indexOf(color);
+                {imageDetails.colors_rgb.map((color) => {
+                    const keyId = imageDetails.colors_rgb.indexOf(color);
                     return (
-                        <Text key={keyId} style={styles.detailItem}>{color}</Text>
+                        <Text key={keyId} style={styles.detailItem}>
+                            {color}
+                        </Text>
                     );
                 })}
 
+                {dataState && (
+                    <PieChart
+                        data={dataState}
+                        width={Dimensions.get("screen").width}
+                        height={200}
+                        chartConfig={chartConfig}
+                        accessor={"population"}
+                        backgroundColor={"transparent"}
+                        paddingLeft={"15"}
+                    //center={[10, 50]}
+                    // absolute
+                    />
+                )}
+                <Text>bobec</Text>
             </View>
         </ScrollView>
     );
